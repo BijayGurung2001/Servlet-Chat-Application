d4501544b369a80e0067df112c18268ff64fe5af
BijayGurung2001 <85681343+BijayGurung2001@users.noreply.github.com>
2024-02-18 22:00:58 +0545

    cleaning code in app.js file

--- client/realchat/src/App.jsx
+++ client/realchat/src/App.jsx
@@ -1,26 +1,17 @@
 import Home from '../Pages/Home'
 import './App.css'
-import io from 'socket.io-client'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { useState } from 'react';
 
-const Socket=io.connect('http://localhost:5000');
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 function App() {
-  const [username,setUsername]=useState('');
-  const [room, setRoom]=useState('')
+
   return (
     <>
       <Router>
         <div className="App">
           <Routes>
             <Route path='/' element={
-              <Home
-            username={username}
-            setUsername={setUsername}
-            room={room}
-            setRoom={setRoom}
-            Socket={Socket}
-            />} />
+              <Home/>} />
           </Routes>
         </div>
       </Router>
